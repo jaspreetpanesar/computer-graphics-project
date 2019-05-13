637181cca7038ded9f34a47343b40cbad5db1ad3
jaspreetpanesar <Jaspreet.Panesar@student.uts.edu.au>
2019-05-13 14:44:45 +1000

    added custom camera class

---
+++ src/js/camera.js
@@ -0,0 +1,27 @@
+
+
+class CustomCamera {
+
+    state = "orbit" // orbit or full
+
+    constructor() {
+        this.camera = new THREE.PerspectiveCamera(
+        this.camera.position.set(0, 0, 200);
+    }
+
+
+    update() {
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
